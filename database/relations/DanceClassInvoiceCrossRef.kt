9a7a6c55f5e298a3cb40d57806976ab9f6ea71ac
des267 <118321083+des267@users.noreply.github.com>
2023-10-10 12:07:09 -0600

    Delete database/relations directory

--- database/relations/DanceClassInvoiceCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.invoice.database.relations
-
-import androidx.room.Entity
-
-@Entity(primaryKeys = ["classId", "invoiceId"])
-data class DanceClassInvoiceCrossRef(
-    val classId: Int,
-    val invoiceId: Int
-)
