9b79d246ee10f9b9d9e55c5ec880331c7d1e5c7b
Gabriel Jorand <gabrieljorand@gmail.com>
2023-11-02 16:50:09 -0600

    implemented some search by features

--- OfficialAndroidApp/app/src/main/java/com/example/javaapp/ClientView.java
+++ OfficialAndroidApp/app/src/main/java/com/example/javaapp/ClientView.java
@@ -1,21 +1,29 @@
 package com.example.javaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.example.javaapp.database.DatabaseHelper;
 import com.example.javaapp.database_v2.ClientModel;
 import com.example.javaapp.database_v2.DatabaseDao;
 
 import java.util.List;
 
 public class ClientView extends AppCompatActivity {
-    Button addClientsBtn, addClassBtn, addInvoiceBtn,viewInvoiceBtn, viewClassBtn;
+    Button addClientsBtn, addClassBtn, addInvoiceBtn,viewInvoiceBtn, viewClassBtn,
+            searchClientBtn, showAllOwing;
+    EditText searchClientEmail, searchClientLastName, searchClientFirstName;
+    List<ClientModel> clientModelList;
+    ArrayAdapter<ClientModel> clientArrayAdapter;
     ListView lv_clientList;
+    DatabaseDao databaseDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,22 +54,54 @@ public class ClientView extends AppCompatActivity {
             startActivity(new Intent(ClientView.this, DanceClassView.class));
         });
         //Navigation Bar End ***********************************************************************
-        //View the Database
-//        DatabaseHelper databaseHelperClient = new DatabaseHelper(ClientView.this);
-//        List<ClientModel> everyClient = databaseHelperClient.getAllClients();
-//        ArrayAdapter clientArrayAdapter = new ArrayAdapter<ClientModel>(ClientView.this,
-//                android.R.layout.simple_list_item_1, everyClient);
-//        lv_clientList.setAdapter(clientArrayAdapter);
-        DatabaseDao databaseDao = new DatabaseDao(ClientView.this);
-        List<ClientModel> everyClient = databaseDao.getAllClients();
-        ArrayAdapter clientArrayAdapter = new ArrayAdapter<ClientModel>(ClientView.this,
-                android.R.layout.simple_list_item_1, everyClient);
+        databaseDao = new DatabaseDao(ClientView.this);
+        clientModelList = databaseDao.getAllClients();
+        clientArrayAdapter = new ArrayAdapter<ClientModel>(ClientView.this,
+                android.R.layout.simple_list_item_1, clientModelList);
         lv_clientList.setAdapter(clientArrayAdapter);
+        //-------------search button on click---------------------------------------------------
+        searchClientBtn = findViewById(R.id.searchClientBtn);
+        searchClientEmail = findViewById(R.id.searchClientEmail);
+        searchClientFirstName = findViewById(R.id.searchClientFirstName);
+        searchClientLastName = findViewById(R.id.searchClientLastName);
+        searchClientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText((Context) ClientView.this, (CharSequence) databaseDao.getOneClientByPrimaryKey(
+//                                searchClientEmail.getText().toString()),
+//                        Toast.LENGTH_LONG).show();
+                try {
+                    // Create client data object from getting info from text boxes
+                    if (searchClientLastName.getText().toString().isEmpty() &&
+                            searchClientFirstName.getText().toString().isEmpty()){
+                        clientModelList.clear();
+                        clientModelList.add(databaseDao.getOneClientByPrimaryKey(
+                                searchClientEmail.getText().toString()));
 
+                        clientArrayAdapter = new ArrayAdapter<ClientModel>(ClientView.this,
+                                android.R.layout.simple_list_item_1, clientModelList);
+                        lv_clientList.setAdapter(clientArrayAdapter);
+                    }
+//                    boolean i = databaseDao.addOneClient(clientModel);
+//                    if (!i) {
+//                        Toast.makeText(AddClient.this, "Failed", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        Toast.makeText(AddClient.this, "Successfully added", Toast.LENGTH_SHORT).show();
+//                        clientFirstNameTextBox.getText().clear();
+//                        clientLastNameTextBox.getText().clear();
+//                        clientEmailTextBox.getText().clear();
+//                        clientPhoneNumberTextBox.getText().clear();
+//                    }
+                } catch (Exception e) {
+                    Toast.makeText(ClientView.this, "Not Found",
+                            Toast.LENGTH_SHORT).show();
+                }
+                searchClientEmail.getText().clear();
+                searchClientFirstName.getText().clear();
+                searchClientLastName.getText().clear();
 
-
-
-
+            }
+        });
         //------------------------------------------------------------------------------------------
     }
 }
